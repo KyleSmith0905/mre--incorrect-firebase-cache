7064553dba3e77de866197201ac432ec1cc0991e
kylesmith090502 <me@yskkyle.com>
2024-01-04 12:20:55 -0500

    Expanded upon MRE to include examples for filtered results.

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,99 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import './index.css';
 import { firestore } from './firestore';
 import { onSnapshot, collection, DocumentData, QuerySnapshot, query, Timestamp, orderBy, QueryDocumentSnapshot, startAfter, limit } from 'firebase/firestore';
 
 // The database model to article
 type Article = {
-  header: string
-  color: string
-  createdAt: Timestamp
-  views: number
-}
+	header: string;
+	color: string;
+	createdAt: Timestamp;
+	views: number;
+};
 
 function App() {
-  const [popular, setPopular] = useState<(Article & {id: string})[]>([]);
-  const [page1, setPage1] = useState<(Article & {id: string})[]>([]);
-  const [page2, setPage2] = useState<(Article & {id: string})[]>([]);
+	const [popular, setPopular] = useState<(Article & { id: string })[]>([]);
+	const [all, setAll] = useState<(Article & { id: string })[]>([]);
+	const [filtered1, setFiltered1] = useState<(Article & { id: string })[]>([]);
+	const [filtered2, setFiltered2] = useState<(Article & { id: string })[]>([]);
 
-  // Retrieve the data and id from the queries
-  const formatArticle = (articles: QuerySnapshot<DocumentData, DocumentData>): (Article & {id: string})[] => {
-    return articles.docs.map(e => {
-      const data = e.data() as Article;
-      return {
-        ...data,
-        id: e.id,
-      }
-    })
-  }
+	// Retrieve the data and id from the queries
+	const formatArticle = (articles: QuerySnapshot<DocumentData, DocumentData>): (Article & { id: string })[] => {
+		return articles.docs.map((e) => {
+			const data = e.data() as Article;
+			return {
+				...data,
+				id: e.id,
+			};
+		});
+	};
 
-  useEffect(() => {
-    // Save the end of the 1st page to paginate
-    let page1EndDocument: QueryDocumentSnapshot<DocumentData, DocumentData>
+	useEffect(() => {
+		// Save the end of the 1st page to paginate
+		let page1EndDocument: QueryDocumentSnapshot<DocumentData, DocumentData>;
 
-    // Get popular articles
-    onSnapshot(query(collection(firestore, 'articles'), orderBy('views', 'desc'), limit(10)), (articles) => {
-      console.log('popular', articles);
-      setPopular(formatArticle(articles))
-    })
+		// Get popular articles
+		onSnapshot(query(collection(firestore, 'articles'), orderBy('views', 'desc'), limit(10)), (articles) => {
+			console.log('popular', articles);
+			setPopular(formatArticle(articles));
+		});
 
-    // Get page 1 of latest articles
-    onSnapshot(query(collection(firestore, 'articles'), orderBy('createdAt', 'desc'), limit(5)), (articles) => {
-      page1EndDocument = articles.docs[articles.docs.length - 1]
-      console.log('page 1', articles);
-      setPage1(formatArticle(articles))
-    })
+		// Get all recent articles
+		onSnapshot(query(collection(firestore, 'articles'), orderBy('createdAt', 'desc'), limit(10)), (articles) => {
+			page1EndDocument = articles.docs[articles.docs.length - 1];
+			console.log('page 1', articles);
+			setAll(formatArticle(articles));
+		});
 
-    // Wait 2.5s for previous databases to resolve and cache
-    setTimeout(() => {
-      // Get page 2 of latest articles
-      onSnapshot(query(collection(firestore, 'articles'), orderBy('createdAt', 'desc'), startAfter(page1EndDocument), limit(5)), (articles) => {
-        console.log('page 2', articles);
-        setPage2(formatArticle(articles))
-      })
-    }, 2500)
-  }, [])
+		// Get page 1 of filtered articles
+		onSnapshot(query(collection(firestore, 'articles'), orderBy('createdAt', 'desc'), limit(5)), (articles) => {
+			page1EndDocument = articles.docs[articles.docs.length - 1];
+			console.log('page 1', articles);
+			setFiltered1(formatArticle(articles));
+		});
 
-  return (
-    <div className='pb-48'>
-      <h1 className='bg-black text-white text-2xl'>Popular videos!</h1>
-      {popular.map((e) => (
-        <p key={e.id} style={{backgroundColor: e.color}}>{e.header}</p>
-      ))}
-      <h1 className='bg-black text-white text-2xl'>Latest videos!</h1>
-      {page1.map((e) => (
-        <p key={e.id} style={{backgroundColor: e.color}}>{e.header}</p>
-      ))}
-      {page2.map((e) => (
-        <p key={e.id} style={{backgroundColor: e.color}}>{e.header}</p>
-      ))}
-    </div>
-  )
+		// Wait 2.5s for previous databases to resolve and cache
+		setTimeout(() => {
+			// Get page 2 of filtered articles
+			onSnapshot(query(collection(firestore, 'articles'), orderBy('createdAt', 'desc'), startAfter(page1EndDocument), limit(5)), (articles) => {
+				if (articles.metadata.fromCache === false && articles.metadata.hasPendingWrites === false) {
+					console.log('page 2', articles);
+					setFiltered2(formatArticle(articles));
+				} else {
+					console.log('page 2: cached page');
+				}
+			});
+		}, 2500);
+	}, []);
+
+	return (
+		<div className='pb-48'>
+			<h1 className='bg-black text-white text-2xl'>Popular videos!</h1>
+			{popular.map((e) => (
+				<p key={e.id} style={{ backgroundColor: e.color }}>
+					{e.header}
+				</p>
+			))}
+			<h1 className='bg-black text-white text-2xl'>Latest videos!</h1>
+			{all.map((e) => (
+				<p key={e.id} style={{ backgroundColor: e.color }}>
+					{e.header}
+				</p>
+			))}
+			<h1 className='bg-black text-white text-2xl'>Filtered videos! (page 1)</h1>
+			{filtered1.map((e) => (
+				<p key={e.id} style={{ backgroundColor: e.color }}>
+					{e.header}
+				</p>
+			))}
+			<h1 className='bg-black text-white text-2xl'>Filtered videos! (page 2)</h1>
+			{filtered2.map((e) => (
+				<p key={e.id} style={{ backgroundColor: e.color }}>
+					{e.header}
+				</p>
+			))}
+		</div>
+	);
 }
 
-export default App
+export default App;
